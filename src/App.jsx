8c35aeb7120159415f89534ac76c38fed62bde0c
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-01-09 00:05:28 -1000

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      3D Developer Portfolio
+    </div>
+  )
+}
+
+export default App
