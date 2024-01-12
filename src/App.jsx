64b4b8b1f1a1dacf17fbfc540d457c488cba6843
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2024-01-12 00:32:26 -1000

    Add layout, add logo to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
+
 
 const Navbar = () => {
+  const [active, setActive] = useState("")
+
   return (
-    <div>Navbar</div>
+    <nav
+      className={`${styles.paddinX} w-full flex-tems-center py-5 fixed top-0 z-20 bg-primary`}>
+        <div className="w-full flex justify-between-tems-center max-w-7xl mx-auto">
+          <Link 
+            to="/" 
+            className="flex items-center gap-2"
+            onClick={() => {
+              setActive("")
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold text-xl cursor-pointer">Jason <span className='sm:block hidden'>| JavaScript Mastery </span></p>
+          </Link>
+        </div>
+    </nav>
   )
 }
 
