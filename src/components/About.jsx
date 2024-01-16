0cd8cba81759ccfc7c12a7089cf3f1245429d974
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2024-01-15 14:53:14 -1000

    Import react-parallex-tilt, and add higher order component to wrap different sections wiith motion and padding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Tilt } from 'react-tilt'
-import { motion } from 'framer-motion'
+import React from "react"
+import Tilt from "react-parallax-tilt"
+import { motion } from "framer-motion"
 
-import { styles } from '../styles'
-import { services } from '../constants'
-import { fadeIn, textVariant } from '../utils/motion'
+import { styles } from "../styles"
+import { services } from "../constants"
+import { SectionWrapper } from "../hoc"
+import { fadeIn, textVariant } from "../utils/motion"
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
@@ -46,11 +47,12 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I&apos;m a Computer Sceience undergrad with experience in JavaScript,
-        and expertise in frameworks like React and Node.js. I&apos;m a quick
-        learner and collaborate closely with my team members to create solutions
-        that solve real-world problems. Let&apos;s work together to bring your
-        ideas to life!
+        I&apos;m a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I&apos;m a quick learner and collaborate closely with clients
+        to create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let&apos;s work together to bring your ideas to
+        life!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
@@ -65,4 +67,5 @@ const About = () => {
     </>
   )
 }
-export default About
+
+export default SectionWrapper(About, "about")
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,34 +1,32 @@
-import React, { Suspense, useEffect, useState, useRef } from "react"
-import { Canvas, useFrame } from "@react-three/fiber"
+import React, { Suspense, useEffect, useState } from "react"
+import { Canvas } from "@react-three/fiber"
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 
 import CanvasLoader from "../Loader"
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./lost_programmer/scene.gltf")
-
+  const computer = useGLTF("./desktop_pc/scene.gltf")
 
   return (
     <mesh>
       <hemisphereLight
-        intensity={1}
-        groundColor="white"
+        intensity={0.15}
+        groundColor="black"
       />
-      <ambientLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={300}
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 1}
-        position={isMobile ? [0, -0.625, 0] : [0, -3.75, 0]}
-        rotation={[4, 0, 0]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
