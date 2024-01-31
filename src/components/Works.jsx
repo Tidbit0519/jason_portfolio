e38c605a54b1e6845d99e086b4791575ad227762
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-01-30 23:08:00 -1000

    Revert "Add project cards animation"
    This reverts commit 29239d928d17d4c9c879862688f3cb43d37e8b49.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,7 +10,6 @@ import { fadeIn, textVariant } from "../utils/motion"
 
 const ProjectCard = ({
   name,
-  index,
   description,
   tags,
   image,
@@ -18,7 +17,6 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max: 45,
@@ -39,7 +37,7 @@ const ProjectCard = ({
               className="bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
-                src={link_type == "github" ? github : external}
+                src={link_type == "github"? github : external}
                 alt="github"
                 className="w-1/2 h-1/2object-contain"
               />
@@ -63,7 +61,6 @@ const ProjectCard = ({
           ))}
         </div>
       </Tilt>
-    </motion.div>
   )
 }
 
