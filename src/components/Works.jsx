a96128fb24f19db36af9ced5925695f9e6aae080
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-01-31 00:03:06 -1000

    Render animation only on desktops

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Tilt } from "react-tilt"
 import { motion } from "framer-motion"
 
@@ -6,6 +6,7 @@ import { styles } from "../styles"
 import { github, external } from "../assets"
 import { SectionWrapper } from "../hoc"
 import { gameProjects, webProjects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion";
 
 const ProjectCard = ({
   name,
@@ -16,54 +17,77 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt={name}
-            className="w-full h-full object-cover rounded-2xl"
-          />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={link_type == "github"? github : external}
-                alt="github"
-                className="w-1/2 h-1/2object-contain"
-              />
-            </div>
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+    >
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src={link_type == "github"? github : external}
+              alt="github"
+              className="w-1/2 h-1/2object-contain"
+            />
           </div>
         </div>
+      </div>
 
-        <div className="mt-5">
-          <h3 className="text-white text-[24px] font-bold">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
+      <div className="mt-5">
+        <h3 className="text-white text-[24px] font-bold">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p
-              key={tag.name}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <p
+            key={tag.name}
+            className={`text-[14px] ${tag.color}`}
+          >
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+    </Tilt>
   )
 }
 
 const Works = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)")
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches)
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange)
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+    }
+  }, [])
+
   return (
     <>
       <p className={`${styles.sectionSubText} text-center`}>My work</p>
@@ -72,8 +96,8 @@ const Works = () => {
       <div className="w-full flex">
         <div className="mt-3 text-secondary text-[17px] max-w-3xl leading=[30px]">
           Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described
-          with links to code repositories and live demos in it. It reflects my
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
         </div>
@@ -85,13 +109,25 @@ const Works = () => {
         Web Development
       </p>
       <div className="flex flex-wrap gap-7">
-        {webProjects.map((project, index) => (
-          <ProjectCard
-            key={`project-${index}`}
-            index={index}
-            {...project}
-          />
-        ))}
+        {webProjects.map((project, index) =>
+          !isMobile ? (
+            <motion.div
+              variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+              key={`project-${index}`}
+            >
+              <ProjectCard
+                index={index}
+                {...project}
+              />
+            </motion.div>
+          ) : (
+            <ProjectCard
+              key={`project-${index}`}
+              index={index}
+              {...project}
+            />
+          )
+        )}
       </div>
 
       <p
@@ -101,11 +137,23 @@ const Works = () => {
       </p>
       <div className="flex flex-wrap gap-7">
         {gameProjects.map((project, index) => (
-          <ProjectCard
-            key={`project-${index}`}
-            index={index}
-            {...project}
-          />
+          !isMobile ? (
+            <motion.div
+              variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+              key={`project-${index}`}
+            >
+              <ProjectCard
+                index={index}
+                {...project}
+              />
+            </motion.div>
+          ) : (
+            <ProjectCard
+              key={`project-${index}`}
+              index={index}
+              {...project}
+            />
+          )
         ))}
       </div>
     </>
