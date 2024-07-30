e8e457ac98e189d7c3eb610793679cbc387a29e7
Jason Ban Tze Tan <jtbt1218@gmail.com>
2024-07-30 11:25:00 -1000

    Update SectionWrapper.jsx

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 import { styles } from "../styles"
 import { staggerContainer } from "../utils/motion"
 
-const StarWrapper = (Component, idName) =>
+const SectionWrapper = (Component, idName) =>
   function HOC() {
     return (
       <motion.section
@@ -25,4 +25,4 @@ const StarWrapper = (Component, idName) =>
     )
   }
 
-export default StarWrapper
+export default SectionWrapper
