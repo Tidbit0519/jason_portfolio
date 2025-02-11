ec7e5e0098875461430cf6b4e58ff04cfd7e002a
Jason Ban Tze Tan <jtbt1218@gmail.com>
2025-02-11 09:54:55 -0700

    Update introduction text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,30 @@
-import React from "react"
-import { motion } from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 
-import { styles } from "../styles"
-import { SectionWrapper } from "../hoc"
-import { fadeIn, textVariant } from "../utils/motion"
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview.</h2>
+			</motion.div>
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
-        I&apos;m an Undergraduate in Computer Science at Brigham Young University-Hawaii, expecting to graduate in Dec 2024. I&apos;m a quick learner and an efficient team-worker, and I aim to create efficient, and user-friendly solutions that solve real-world problems. Let&apos;s work together to bring your ideas to life!
-      </motion.p>
-    </>
-  )
-}
+			<motion.p
+				variants={fadeIn("", "", 0.1, 1)}
+				className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+			>
+				<span className="font-bold">B.S. in Computer Science </span>at Brigham Young
+				University-Hawaii, graduated December 2024. A quick learner and an efficient
+				team-worker, passionate about creating efficient, and user-friendly solutions
+				that solve real-world problems. Experience in full-stack development with tools
+				like <span className="font-bold">React, Node.js, ASP.NET Core, and more. </span>
+			</motion.p>
+		</>
+	);
+};
 
-export default SectionWrapper(About, "about")
+export default SectionWrapper(About, "about");
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,55 +1,55 @@
-import { motion } from "framer-motion"
-import { styles } from "../styles"
-import { SectionWrapper } from "../hoc"
-import { ProgrammerCanvas } from "./canvas"
-import { textVariant, fadeIn } from "../utils/motion"
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { ProgrammerCanvas } from "./canvas";
+import { textVariant, fadeIn } from "../utils/motion";
 
 const Hero = () => {
-  return (
-    <section className={`relative w-full h-screen mx-auto sm:pb-72`}>
-      <div
-        className={`inset-0 top-[50px] max-w-full mx-auto ${styles.paddingX} flex flex-row items-start gap-5 sm:mb-4 -mb-24`}
-      >
-        <div className="flex flex-col justify-center items-center mt-5"></div>
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I&apos;m <span className="text-[#915eff]">Jason</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am aspiring to be a software developer/engineer.
-          </p>
-        </div>
-      </div>
+	return (
+		<section className={`relative w-full h-screen mx-auto sm:pb-72`}>
+			<div
+				className={`inset-0 top-[50px] max-w-full mx-auto ${styles.paddingX} flex flex-row items-start gap-5 sm:mb-4 -mb-24`}
+			>
+				<div className="flex flex-col justify-center items-center mt-5"></div>
+				<div>
+					<h1 className={`${styles.heroHeadText} text-white`}>
+						Hi, I&apos;m <span className="text-[#915eff]">Jason</span>
+					</h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
+						An aspiring software developer/engineer.
+					</p>
+				</div>
+			</div>
 
-      <ProgrammerCanvas />
+			<ProgrammerCanvas />
 
-      <div className="sm:sticky relative sm:bottom-12 bottom-72 w-full flex justify-center items-center">
-        <a href="#about">
-          <motion.p
-            variants={fadeIn("", "", 0.1, 1)}
-            className="mt-4 text-secondary text-[14px] max-w-3xl leading-[30px] p-2"
-          >
-            Scroll down to learn more about me!
-          </motion.p>
-          <div className="flex justify-center items-center">
-            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-              <motion.dev
-                animate={{
-                  y: [0, 24, 0],
-                }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: "loop",
-                }}
-                className="w-3 h-3 rounded-full bg-secondary mb-1"
-              />
-            </div>
-          </div>
-        </a>
-      </div>
-    </section>
-  )
-}
+			<div className="sm:sticky relative sm:bottom-12 bottom-72 w-full flex justify-center items-center">
+				<a href="#about">
+					<motion.p
+						variants={fadeIn("", "", 0.1, 1)}
+						className="mt-4 text-secondary text-[14px] max-w-3xl leading-[30px] p-2"
+					>
+						Scroll down to learn more about me!
+					</motion.p>
+					<div className="flex justify-center items-center">
+						<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+							<motion.dev
+								animate={{
+									y: [0, 24, 0],
+								}}
+								transition={{
+									duration: 1.5,
+									repeat: Infinity,
+									repeatType: "loop",
+								}}
+								className="w-3 h-3 rounded-full bg-secondary mb-1"
+							/>
+						</div>
+					</div>
+				</a>
+			</div>
+		</section>
+	);
+};
 
-export default SectionWrapper(Hero, "")
+export default SectionWrapper(Hero, "");
