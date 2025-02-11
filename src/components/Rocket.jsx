f6f13a4a40c01ff6a614d05068aef2845b4b005f
Jason Ban Tze Tan <jtbt1218@gmail.com>
2025-02-11 09:55:16 -0700

    Fix rocket img z-index

--- src/components/Rocket.jsx
+++ src/components/Rocket.jsx
@@ -1,104 +1,100 @@
-import React, { useEffect, useRef, useState } from "react"
-import { Tooltip } from "react-tooltip"
-import { rocket } from "../assets"
+import React, { useEffect, useRef, useState } from "react";
+import { Tooltip } from "react-tooltip";
+import { rocket } from "../assets";
 
-const offset = 65
+const offset = 65;
 
 const Rocket = () => {
-  const rocketRef = useRef(null)
+	const rocketRef = useRef(null);
 
-  const [rotation, setRotation] = useState(0)
-  const [position, setPosition] = useState({
-    left: window.innerWidth * 0.75,
-    top: window.innerHeight * 0.25,
-  })
-  const [positionDrag, setPositionDrag] = useState({
-    left: window.innerWidth / 0.75,
-    top: window.innerHeight * 0.25,
-  })
+	const [rotation, setRotation] = useState(0);
+	const [position, setPosition] = useState({
+		left: window.innerWidth * 0.75,
+		top: window.innerHeight * 0.25,
+	});
+	const [positionDrag, setPositionDrag] = useState({
+		left: window.innerWidth / 0.75,
+		top: window.innerHeight * 0.25,
+	});
 
-  const updateRotation = (event) => {
-    const posX =
-      event.clientX -
-      (rocketRef.current.getBoundingClientRect().left +
-        rocketRef.current.width / 2)
-    const posY =
-      event.clientY -
-      (rocketRef.current.getBoundingClientRect().top +
-        rocketRef.current.height / 2)
+	const updateRotation = (event) => {
+		const posX =
+			event.clientX -
+			(rocketRef.current.getBoundingClientRect().left + rocketRef.current.width / 2);
+		const posY =
+			event.clientY -
+			(rocketRef.current.getBoundingClientRect().top + rocketRef.current.height / 2);
 
-    const rad = Math.atan2(posY, posX)
-    const deg = rad * (180 / Math.PI) + 180 - offset
+		const rad = Math.atan2(posY, posX);
+		const deg = rad * (180 / Math.PI) + 180 - offset;
 
-    setRotation(deg)
-  }
+		setRotation(deg);
+	};
 
-  const updatePosition = (event) => {
-    setPosition({
-      left: event.clientX - rocketRef.current.width / 2 + window.scrollX,
-      top: event.clientY - rocketRef.current.height / 2 + window.scrollY,
-    })
-  }
+	const updatePosition = (event) => {
+		setPosition({
+			left: event.clientX - rocketRef.current.width / 2 + window.scrollX,
+			top: event.clientY - rocketRef.current.height / 2 + window.scrollY,
+		});
+	};
 
-  useEffect(() => {
-    window.addEventListener("pointermove", updateRotation)
-    window.addEventListener("pointerdown", updateRotation)
-    window.addEventListener("pointerdown", updatePosition)
+	useEffect(() => {
+		window.addEventListener("pointermove", updateRotation);
+		window.addEventListener("pointerdown", updateRotation);
+		window.addEventListener("pointerdown", updatePosition);
 
-    return () => {
-      window.removeEventListener("pointermove", updateRotation)
-      window.addEventListener("pointerdown", updateRotation)
-      window.removeEventListener("pointerdown", updatePosition)
-    }
-  }, [])
+		return () => {
+			window.removeEventListener("pointermove", updateRotation);
+			window.addEventListener("pointerdown", updateRotation);
+			window.removeEventListener("pointerdown", updatePosition);
+		};
+	}, []);
 
-  useEffect(() => {
-    let request
+	useEffect(() => {
+		let request;
 
-    const animateRocket = () => {
-      setPositionDrag((prevPositionDrag) => ({
-        left:
-          prevPositionDrag.left + (position.left - prevPositionDrag.left) / 15,
-        top: prevPositionDrag.top + (position.top - prevPositionDrag.top) / 15,
-      }))
+		const animateRocket = () => {
+			setPositionDrag((prevPositionDrag) => ({
+				left: prevPositionDrag.left + (position.left - prevPositionDrag.left) / 15,
+				top: prevPositionDrag.top + (position.top - prevPositionDrag.top) / 15,
+			}));
 
-      request = requestAnimationFrame(animateRocket)
-    }
+			request = requestAnimationFrame(animateRocket);
+		};
 
-    animateRocket()
+		animateRocket();
 
-    return () => {
-      cancelAnimationFrame(request)
-    }
-  }, [position])
+		return () => {
+			cancelAnimationFrame(request);
+		};
+	}, [position]);
 
+	return (
+		<>
+			<Tooltip
+				className="z-10"
+				anchorSelect="#rocket"
+				content="Move me!"
+				defaultIsOpen="true"
+				openOnClick="true"
+				offset={-10}
+			/>
+			<img
+				id="rocket"
+				className="absolute w-[100px] z-10"
+				ref={rocketRef}
+				src={rocket}
+				alt="Rocket"
+				style={{
+					transform: `rotate(${rotation}deg)`,
+					left: `${positionDrag.left}px`,
+					top: `${positionDrag.top}px`,
+				}}
+				data-tooltip-variant="dark"
+				data-tooltip-place="left"
+			/>
+		</>
+	);
+};
 
-  return (
-    <>
-      <Tooltip
-        className="z-50"
-        anchorSelect="#rocket"
-        content="Move me!"
-        defaultIsOpen="true"
-        openOnClick="true"
-        offset={-10}
-      />
-      <img
-        id="rocket"
-        className="absolute w-[100px] z-50"
-        ref={rocketRef}
-        src={rocket}
-        alt="Rocket"
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          left: `${positionDrag.left}px`,
-          top: `${positionDrag.top}px`,
-        }}
-        data-tooltip-variant="dark"
-        data-tooltip-place="left"
-      />
-    </>
-  )
-}
-
-export default Rocket
+export default Rocket;
