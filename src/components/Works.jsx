c947e46646a3c501d2503b29776def71a01f3418
Jason Ban Tze Tan <jtbt1218@gmail.com>
2025-02-12 10:39:51 -0700

    Fix project description layout

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -32,21 +32,18 @@ const ProjectCard = ({
 						alt={name}
 						className="w-full h-full object-cover rounded-2xl"
 					/>
-        ) :
-          <div className="w-full h-full bg-secondary rounded-2xl">
-            <p className="text-white text-2xl flex justify-center pt-24">Coming Soon...</p>
-          </div>}
+				) : (
+					<div className="w-full h-full bg-secondary rounded-2xl">
+						<p className="text-white text-2xl flex justify-center pt-24">Coming Soon...</p>
+					</div>
+				)}
 				<div className="absolute inset-0 flex justify-end m-3 card-img_hover">
 					<div
 						onClick={() => window.open(source_code_link, "_blank")}
 						className="bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
 					>
 						<img
-							src={
-								link_type && link_type == "github"
-									? github
-									: external
-							}
+							src={link_type && link_type == "github" ? github : external}
 							alt="github"
 							className="w-1/2 h-1/2object-contain"
 						/>
@@ -100,18 +97,14 @@ const Works = () => {
 	return (
 		<>
 			<p className={`${styles.sectionSubText} text-center`}>My work</p>
-			<h2 className={`${styles.sectionHeadText} text-center`}>
-				Projects
-			</h2>
+			<h2 className={`${styles.sectionHeadText} text-center`}>Projects</h2>
 
 			<div className="w-full flex">
-				<div className="mt-3 text-secondary text-[17px] max-w-3xl leading=[30px]">
-					Following projects showcases my skills and experience
-					through real-world examples of my work. Each project is
-					briefly described with links to code repositories and live
-					demos in it. It reflects my ability to solve complex
-					problems, work with different technologies, and manage
-					projects effectively.
+				<div className="mt-3 text-secondary text-[17px] mx-0 xs:mx-32 text-center">
+					Following projects showcases my skills and experience through real-world
+					examples of my work. Each project is briefly described with links to code
+					repositories and live demos in it. It reflects my ability to solve complex
+					problems, work with different technologies, and manage projects effectively.
 				</div>
 			</div>
 
